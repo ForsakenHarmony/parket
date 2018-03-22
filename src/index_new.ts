4e1b1bd2515c9edf22ec2eb9bdc7fb684b7aa71e
Leah <ulliftw@gmail.com>
2018-03-22 01:31:50 +0100

    potential 1.0 candidate, single object model

---
+++ src/index_new.ts
@@ -0,0 +1,301 @@
+import { apc, modelName, modelSymbol, parentSymbol, vpc } from './symbols';
+import mitt, { EventHandler, Emitter } from './emitter';
+
+export type Event = {
+  path?: string;
+  value?: string;
+  [index: string]: any;
+};
+
+export type EmitFn = (evt: string, val: Event) => void;
+
+export interface ModelCommons<S> {
+  onAction: (fn: EventHandler, after?: boolean) => Function;
+  onSnapshot: (fn: EventHandler) => Function;
+  onPatch: (fn: EventHandler) => Function;
+  getSnapshot: () => object;
+  applySnapshot: (snapshot: object, dontemit?: boolean) => void;
+  getParent: () => Model<any> | null;
+  getRoot: () => Model<any>;
+
+  [modelName]: string;
+  [modelSymbol]: boolean;
+  [parentSymbol]: (emit: EmitFn, _parent: Model<S>, _path: string) => void;
+}
+
+export type Model<S> = ModelCommons<S> & {
+  [vpc]: Model<S>;
+  [apc]: Model<S>;
+  [index: string]: any;
+} & S;
+
+export type Init<S> = {
+  [index: string]:
+    | ((this: Model<S>, ...args: any[]) => any)
+    | string
+    | number
+    | object;
+};
+
+export type InitFn<K, S extends Init<K>> = (obj?: any) => S;
+
+export type Obj = { [index: string]: any };
+
+const objKeys = Object.keys.bind(Object);
+function diff<S>(
+  oldObj: Model<S>,
+  newObj: { [index: string]: any },
+  whitelist: string[]
+) {
+  if (oldObj === newObj) return;
+  objKeys(oldObj)
+    .reduce(
+      (acc: string[], val: string) =>
+        ~acc.indexOf(val) ? acc : acc.concat(val),
+      objKeys(newObj)
+    )
+    .forEach((key: string) => {
+      if (~whitelist.indexOf(key)) return;
+      const oldVal = oldObj[key];
+      const newVal = newObj[key];
+      if (oldVal === newVal) return;
+      if (
+        oldVal != null &&
+        newVal != null &&
+        typeof oldVal === 'object' &&
+        typeof newVal === 'object'
+      ) {
+        oldVal[modelSymbol]
+          ? oldVal.applySnapshot(newVal, true)
+          : diff(oldVal, newVal, whitelist);
+      } else {
+        oldObj[apc][key] = newVal;
+      }
+    });
+}
+
+function setUpObject<S>(
+  obj: Model<S>,
+  emit: EmitFn,
+  symbol: Symbol,
+  path: string,
+  parent = obj
+): Model<S> {
+  if (obj[modelName] && !obj[modelSymbol]) {
+    obj = modelMap.has(obj[modelName])
+      ? modelMap.get(obj[modelName])(obj)
+      : obj;
+  }
+  if (obj[modelSymbol] && obj !== parent) {
+    obj[parentSymbol](emit, parent, path);
+    return obj;
+  } else {
+    // have to ignore errors because typescript doesn't like indexing with symbols
+    if (
+      !obj[apc] ||
+      // @ts-ignore
+      !obj[apc][symbol]
+    )
+      obj[apc] = cProxy(obj, emit, symbol, false, path, parent);
+
+    if (
+      !obj[vpc] ||
+      // @ts-ignore
+      !obj[vpc][symbol]
+    )
+      obj[vpc] = cProxy(obj, emit, symbol, true, path, parent);
+  }
+
+  for (let prop in obj) {
+    const val = obj[prop];
+    if (val != null && typeof val === 'object') {
+      obj[prop] = setUpObject(val, emit, symbol, path + '/' + prop, parent);
+    }
+  }
+
+  return obj;
+}
+
+function cProxy<S>(
+  obj: Model<S>,
+  emit: EmitFn,
+  symbol: Symbol,
+  view: boolean,
+  path: string,
+  parent: Model<S>
+) {
+  return new Proxy(obj, {
+    get(target: Model<S>, prop) {
+      if (prop === symbol) return true;
+      if (typeof prop === 'symbol') return target[prop];
+      const res = target[prop];
+      // check if there are other built in constructors that need this
+      if (target instanceof Date && typeof res === 'function')
+        return res.bind(target);
+      return (
+        (res != null &&
+          typeof res === 'object' &&
+          (view ? res[vpc] : res[apc])) ||
+        res
+      );
+    },
+    set(target: Model<S>, prop: string, val) {
+      if (view) {
+        throw new Error("You can't modify the state outside of actions");
+      }
+      if (target[prop] === val) return true;
+      if (typeof prop === 'symbol') {
+        target[prop] = val;
+        return true;
+      }
+      if (val != null && typeof val === 'object') {
+        val = setUpObject(
+          val,
+          emit,
+          symbol,
+          path + '/' + prop,
+          parent || target[vpc]
+        );
+      }
+      target[prop] = val;
+      emit('patch', { path: path + '/' + prop, op: 'replace', value: val });
+      return true;
+    },
+  });
+}
+
+function subscribe(emitter: Emitter, evt: string, fn: EventHandler) {
+  emitter.on(evt, fn);
+  return () => emitter.off(evt, fn);
+}
+
+const modelMap = new Map();
+
+export default function model<S extends Init<any>>(
+  name: string,
+  init: InitFn<S, S>
+): (obj?: Obj) => Model<S> {
+  function instantiate(obj: Obj = {}): Model<S> {
+    const symbol = Symbol('model');
+
+    let path = '';
+    let parentEmit: EmitFn | null = null;
+    let parent: Model<any> | null = null;
+    const emitter = mitt();
+
+    const model = init(obj);
+
+    const emit: EmitFn = (evt, val) => {
+      if (evt === 'snapshot') {
+        val = common.getSnapshot();
+      } else if (val.path != null) {
+        val.path = path + val.path;
+      }
+      emitter.emit(evt, val);
+      parentEmit && parentEmit(evt, val);
+    };
+
+    const whitelist: string[] = [];
+
+    let state = {} as Model<S>;
+
+    const common: ModelCommons<S> = {
+      onAction(fn, after) {
+        const evt = after ? 'action-complete' : 'action';
+        return subscribe(emitter, evt, fn);
+      },
+      onSnapshot(fn) {
+        return subscribe(emitter, 'snapshot', fn);
+      },
+      onPatch(fn) {
+        return subscribe(emitter, 'patch', fn);
+      },
+      getSnapshot() {
+        return JSON.parse(JSON.stringify(state));
+      },
+      applySnapshot(snapshot, dontemit) {
+        diff<S>(state, snapshot, whitelist);
+        !dontemit && emit('snapshot', {});
+      },
+      getParent() {
+        return parent;
+      },
+      getRoot() {
+        return parent ? parent.getRoot() : state;
+      },
+      [modelSymbol]: true,
+      [parentSymbol]: (emit: EmitFn, _parent: Model<any>, _path: string) => {
+        parentEmit = emit;
+        path = _path;
+        parent = _parent;
+      },
+      [modelName]: name,
+    };
+
+    whitelist.push.apply(whitelist, objKeys(common));
+
+    state = setUpObject<S>(common as Model<S>, emit, symbol, '');
+
+    // r = refresh on get, v = value
+    const caches: { [index: string]: { v: any; r: boolean } } = {};
+
+    const emitSnapshot = (name: string, args: any[]) => {
+      emit('action-complete', { name, path: '', args });
+      emit('snapshot', {});
+    };
+
+    for (const key in model) {
+      const prop = Object.getOwnPropertyDescriptor(model, key);
+      if (!prop) continue;
+
+      if (typeof prop.get === 'function') {
+        // view
+        const view = prop.get;
+        Object.defineProperty(state, key, {
+          get: () => {
+            const cache = caches[key];
+            if (cache.r) {
+              cache.r = false;
+              cache.v = view.apply(state[vpc]);
+            }
+            return cache.v;
+          },
+          configurable: false,
+        });
+        caches[key] = { v: undefined, r: true };
+      } else if (typeof prop.value === 'function') {
+        // action
+        const action: Function = prop.value;
+        state[key] = (...args: any[]) => {
+          emit('action', { name: key, path: '', args: args });
+          const res = action.apply(state[apc], args);
+          if (res != null && res.then) {
+            res.then(emitSnapshot.bind(null, key, args));
+          } else {
+            emitSnapshot(key, args);
+          }
+          return res;
+        };
+        whitelist.push(key);
+      } else {
+        // prop
+        state[apc][key] = prop.value;
+      }
+    }
+
+    // update the cache on change
+    state.onPatch(() => {
+      for (const key in caches) {
+        caches[key].r = true;
+      }
+    });
+
+    return state;
+  }
+  modelMap.set(name, instantiate);
+  return obj => instantiate(obj)[vpc];
+}
+
+export function clearCache() {
+  modelMap.clear();
+}
---
+++ test/model_new.test.ts
@@ -0,0 +1,240 @@
+import model, { clearCache } from '../src/index_new';
+
+describe('model()', function() {
+  beforeEach(() => {
+    clearCache();
+  });
+
+  it('should create a model', () => {
+    expect(model('Model', () => ({})));
+  });
+
+  it('should instantiate', () => {
+    const Model = model('Model', () => ({}));
+    const instance = Model();
+
+    expect(instance).toMatchObject({
+      getSnapshot: expect.any(Function),
+      applySnapshot: expect.any(Function),
+      onPatch: expect.any(Function),
+      onAction: expect.any(Function),
+      onSnapshot: expect.any(Function),
+    });
+  });
+
+  it('should instantiate with objects / arrays in initial state', () => {
+    const Model = model('Model', () => ({
+      obj: {
+        test: true,
+      },
+      arr: [1, 2, 3],
+    }));
+    const instance = Model();
+    expect(instance);
+  });
+
+  it('should instantiate with objects / arrays in nested initial state', () => {
+    const Nested = model('Nested', () => ({
+      obj: {
+        test: true,
+      },
+      arr: [1, 2, 3],
+    }));
+
+    const Model = model('Model', () => ({
+      nested: Nested(),
+    }));
+
+    const instance = Model();
+    expect(instance);
+  });
+
+  it('should have props, actions and views', () => {
+    const Person = model('Person', ({ firstname }) => ({
+      firstname,
+      lastname: 'Lennon',
+
+      setFirstName(first: string) {
+        this.firstname = first;
+      },
+      setLastName(last: string) {
+        this.lastname = last;
+      },
+
+      get fullname() {
+        return `${this.firstname} ${this.lastname}`;
+      },
+    }));
+
+    const instance = Person({ firstname: 'John' });
+
+    expect(instance).toMatchObject({
+      firstname: 'John',
+      lastname: 'Lennon',
+
+      setFirstName: expect.any(Function),
+      setLastName: expect.any(Function),
+
+      fullname: 'John Lennon',
+    });
+
+    function test(thing: string) {
+      console.log(thing);
+    }
+
+    const name = instance.fullname;
+
+    test(name);
+
+    instance.setFirstName('Tom');
+    instance.setLastName('Clancy');
+
+    expect(instance.fullname).toBe('Tom Clancy');
+  });
+
+  it('should emit on change', () => {
+    const Model = model('Model', () => ({
+      test: 0,
+
+      increment() {
+        this.test++;
+      },
+    }));
+
+    const instance = Model();
+
+    const pSub = jest.fn();
+    const aSub = jest.fn();
+
+    instance.onPatch(pSub);
+    instance.onAction(aSub);
+
+    instance.increment();
+
+    expect(pSub).toBeCalled();
+    expect(aSub).toBeCalled();
+  });
+
+  it('should get events from nested objects', function() {
+    const Model = model('Model', () => ({
+      nested: {
+        test: 0,
+      },
+      increment() {
+        this.nested.test++;
+      },
+    }));
+
+    const instance = Model();
+
+    const sub = jest.fn();
+    instance.onPatch(sub);
+    instance.increment();
+    expect(sub).toBeCalled();
+  });
+
+  it('should get events from nested models', function() {
+    const Nested = model('Nested', () => ({
+      test: 0,
+      increment() {
+        this.test++;
+      },
+    }));
+
+    const Model = model('Model', () => ({
+      nested: Nested(),
+    }));
+
+    const instance = Model();
+
+    const sub = jest.fn();
+    instance.onPatch(sub);
+    instance.nested.increment();
+    expect(sub).toBeCalled();
+  });
+
+  it('should update on array methods', function() {
+    const Model = model('Model', () => ({
+      arr: [] as any[],
+      pushToArr(thing: any) {
+        this.arr.push(thing);
+      },
+      get firstElem() {
+        return this.arr[0];
+      },
+    }));
+
+    const instance = Model();
+
+    const sub = jest.fn();
+    instance.onPatch(sub);
+    instance.pushToArr(2);
+    expect(sub).toBeCalled();
+    expect(instance.firstElem).toBe(2);
+  });
+
+  it('should handle dates', function() {
+    const Model = model('Model', () => ({
+      date: new Date(),
+    }));
+
+    const instance = Model();
+    instance.getSnapshot();
+    instance.date.getDate();
+  });
+
+  it('should apply snapshots', function() {
+    const Person = model('Person', () => ({
+      firstname: 'John',
+      lastname: 'Lennon',
+      get fullname() {
+        return `${this.firstname} ${this.lastname}`;
+      },
+    }));
+
+    const instance = Person();
+
+    instance.applySnapshot({ firstname: 'Tom', lastname: 'Clancy' });
+
+    expect(instance.fullname).toBe('Tom Clancy');
+  });
+
+  it('should get the root in nested models', () => {
+    debugger;
+    const Model2 = model('M1', () => ({
+      root() {
+        return this.getRoot().thing;
+      },
+    }));
+
+    const Model1 = model('M1', () => ({
+      nest: Model2(),
+      root() {
+        return this.getRoot().thing;
+      },
+    }));
+
+    const Model = model('M', () => ({
+      nest: Model1(),
+      thing: 'root',
+    }));
+
+    const instance = Model();
+
+    expect(instance.nest.root()).toBe('root');
+    expect(instance.nest.nest.root()).toBe('root');
+  });
+
+  // it('should call init when instantiated', () => {
+  //   const init = jest.fn();
+  //
+  //   const Model = model('Model', {
+  //     initial: () => ({}),
+  //     actions: state => ({
+  //       init,
+  //     }),
+  //   });
+  //   Model();
+  //   expect(init).toBeCalled();
+  // });
+});
